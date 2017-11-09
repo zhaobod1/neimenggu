c1cc3cacff96e1b7b7fce5c6828fed2117050402
zhaobo <zhaobod1@163.com>
2017-11-09 10:23:47 +0800

    admin footer changed

---
+++ resources/views/admin/dashboard/title.blade.php
@@ -0,0 +1,35 @@
+<style>
+    .title {
+        font-size: 50px;
+        color: #636b6f;
+        font-family: 'Raleway', sans-serif;
+        font-weight: 100;
+        display: block;
+        text-align: center;
+        margin: 20px 0 10px 0px;
+    }
+
+    .links {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+
+    .links > a {
+        color: #636b6f;
+        padding: 0 25px;
+        font-size: 12px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+</style>
+
+<div class="title">
+    内蒙古信用社小程序管理系统
+</div>
+<div class="links">
+    <a href="https://host.huo15.com" target="_blank">火一五</a>
+    <a href="http://ask.huo15.com"  target="_blank">公司问答</a>
+    <a href="http://www.huo15.com"  target="_blank">官网</a>
+</div>
---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        <strong>Version</strong>&nbsp;&nbsp; {!! config('admin.version') !!}
+    </div>
+    <!-- Default to the left -->
+    <strong>Powered by <a href="https://host.huo15.com" target="_blank">青岛火一五信息科技有限公司</a></strong>
+</footer>
